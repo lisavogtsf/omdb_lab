3b53ab257b02708e52ee5166bd8c38fc8d6613fe
Lisa Vogt <lisavogt.sf@gmail.com>
2014-07-22 17:25:01 -0700

    showing imdbID on details page

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-var express = require("express")
-  request = require('request');
+var express = require('express'),
+  ejs = require('ejs'),
+  request = require('request'),
+  bodyParser = require('body-parser');
 
 var app = express();
+
+app.set("view engine", "ejs");
 
 app.get('/', function(req, res){
   res.render('index.ejs');
@@ -17,8 +21,26 @@ app.get('/search', function(req, res){
       res.render("results.ejs", {movieList: data.Search || []});
 
     }
-
   });
 });
+
+app.get('/movie/:id', function(req, res){
+    // need to catch id and redirect to new page
+    var selectedID = req.params.id;
+    res.render("details", {movieID: selectedID});
+});
+
+
+// var omdbQuery = function(searchTerm){
+//   var searchRequest = req.query.searchTerm;
+//   var searchURL = "http://www.omdbapi.com/?s=" + searchRequest;
+//   request(searchURL, function(error, response, body){
+//     if (!error){
+//       var data = JSON.parse(body); 
+//       res.render("results.ejs", {movieList: data.Search || []});
+
+//     }
+//   });  
+// }
 
 app.listen(3000);
