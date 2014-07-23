07692d25bf46a18f54303f514e1bb05d071dfcc7
Lisa Vogt <lisavogt.sf@gmail.com>
2014-07-22 18:41:29 -0700

    fake favorites page

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var express = require('express'),
   ejs = require('ejs'),
   request = require('request'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  app = express();
 
-var app = express();
+// array with IDs of favorite films
+// var favID = ["tt0151625", "tt0372784"];
+var favObjects = [{
+  Title: "Monty Python & the Quest for the Holy Grail",
+  imdbID: "tt0151625"}, 
+  {Title: "Batman Begins",
+  imdbID: "tt0372784"}];
 
 app.set("view engine", "ejs");
 
@@ -37,17 +44,20 @@ app.get('/movie/:id', function(req, res){
   });
 });
 
+app.get('/favorites', function(req, res){
+  // res.send("search page: " + query);
+  res.render("favorites", {favList: favObjects});
+});
 
-// var omdbQuery = function(searchTerm){
-//   var searchRequest = req.query.searchTerm;
-//   var searchURL = "http://www.omdbapi.com/?s=" + searchRequest;
-//   request(searchURL, function(error, response, body){
-//     if (!error){
-//       var data = JSON.parse(body); 
-//       res.render("results.ejs", {movieList: data.Search || []});
+// app.post("/books", function(req, res){
+//   console.log(req.body);
+//   count += 1;
+//   // res.send("Livre submitted: " + req.body);
+//   var localBook = req.body.livre;
+//   localBook.id = count;
+//   bookList.push(localBook);
+//   res.redirect("/books");
+// });
 
-//     }
-//   });  
-// }
 
 app.listen(3000);
